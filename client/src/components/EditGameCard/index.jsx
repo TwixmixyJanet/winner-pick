80c37b66519f77e56ed870bbcda8682111c44327
Janet Webster <twixmixy@gmail.com>
2024-02-03 11:45:35 -0500

    Update edit game component

--- client/src/components/EditGameCard/index.jsx
+++ client/src/components/EditGameCard/index.jsx
@@ -1,91 +1,91 @@
 import { Input, Ripple, initMDB } from "mdb-ui-kit";
 import { useQuery, useMutation } from "@apollo/client";
-import { useEffect, useRef, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import Auth from '../../utils/auth';
+import { useEffect, useRef, useState } from "react";
+import { useParams } from "react-router-dom";
+import Auth from "../../utils/auth";
 
-import { QUERY_USER, QUERY_RECIPE } from "../../utils/queries";
-import { UPDATE_RECIPE } from "../../utils/mutations";
+import { QUERY_USER, QUERY_GAME } from "../../utils/queries";
+import { UPDATE_GAME } from "../../utils/mutations";
 
 initMDB({ Input, Ripple });
 
-export default function RecipeForm() {
-  const { id: recipeId } = useParams();
+export default function GameForm() {
+  const { id: gameId } = useParams();
   const cloudinaryRef = useRef();
   const widgetRef = useRef();
   const [myImage, setMyImage] = useState();
-  const [userFamlies, setUserFamilies] = useState([]);
+  const [userFamlies, setUserGroups] = useState([]);
   const [uploadError, setuploadError] = useState("");
 
-  const { loading, data, error } = useQuery(QUERY_RECIPE, {
-    variables: { id: recipeId },
+  const { loading, data, error } = useQuery(QUERY_GAME, {
+    variables: { id: gameId },
   });
 
   useEffect(() => {
-      cloudinaryRef.current = window.cloudinary;
-      widgetRef.current = cloudinaryRef.current.createUploadWidget({
-          cloudName: 'defuryakl',
-          uploadPreset: 'reciperolodex',
-      }, function(error, result) {
-          if (result && result.info && result.info.secure_url) {
-              setMyImage(result.info.secure_url);
-          }
-      });
+    cloudinaryRef.current = window.cloudinary;
+    widgetRef.current = cloudinaryRef.current.createUploadWidget(
+      {
+        cloudName: "defuryakl",
+        uploadPreset: "winnerpick",
+      },
+      function (error, result) {
+        if (result && result.info && result.info.secure_url) {
+          setMyImage(result.info.secure_url);
+        }
+      }
+    );
   }, []);
 
-  const [updateRecipe] = useMutation(UPDATE_RECIPE);
+  const [updateGame] = useMutation(UPDATE_GAME);
 
   // const username = "B-King";
-  const username = Auth.getProfile().authenticatedPerson.username
+  const username = Auth.getProfile().authenticatedPerson.username;
   // Update the variables below to be authorized user's username when we get there.
   const { data: userData } = useQuery(QUERY_USER, {
-    variables: { username: username }, 
+    variables: { username: username },
   });
 
   const [formData, setFormData] = useState({
     name: "",
-    cookingTime: "",
-    servingSize: "",
-    instructions: "",
-    ingredients: "",
-    familyId: "", // The selected family id
+    description: "",
+    castMembers: "",
+    numMembers: "",
+    groupId: "", // The selected group id
   });
 
   const handleSubmit = async () => {
     try {
-      console.log( "formData: ", formData);
-      console.log( "myImage: ", myImage);
-      console.log( "username: ", username);
-      const { name, cookingTime, instructions, ingredients, servingSize, familyId } = formData;
-      
-      // Use updateRecipe mutation instead of addRecipe
-      const { data, error } = await updateRecipe({
+      console.log("formData: ", formData);
+      console.log("myImage: ", myImage);
+      console.log("username: ", username);
+      const { name, description, castMembers, numMembers, groupId } = formData;
+
+      // Use updateGame mutation instead of addGame
+      const { data, error } = await updateGame({
         variables: {
-          // Include recipe ID in the variables
-          id: recipeId,
+          // Include game ID in the variables
+          id: gameId,
           name: name,
-          cookingTime: cookingTime,
-          instructions: instructions,
-          ingredients: ingredients,
-          servingSize: servingSize,
-          familyId: familyId,
+          description: description,
+          castMembers: castMembers,
+          numMembers: numMembers,
+          groupId: groupId,
           photo: myImage,
-          author: username
+          author: username,
         },
-        // refetchQueries: [{ query: QUERY_RECIPE, variables: { username } }],
+        // refetchQueries: [{ query: QUERY_GAME, variables: { username } }],
       });
       console.log("data: ", data);
 
-      if( data ){
+      if (data) {
         window.location.replace(window.location.origin + "/dashboard");
       }
 
-      if( error ){
-        setuploadError("something went wrong, please try again")
+      if (error) {
+        setuploadError("something went wrong, please try again");
       }
-
     } catch (error) {
-      console.error("Error submitting recipe:", error);
+      console.error("Error submitting game:", error);
     }
   };
 
@@ -94,51 +94,46 @@ export default function RecipeForm() {
     setFormData({ ...formData, [id]: value });
 
     const inputElement = e.target;
-    if (value.trim() !== '') {
-      inputElement.classList.add('active');
+    if (value.trim() !== "") {
+      inputElement.classList.add("active");
     } else {
-      inputElement.classList.remove('active');
+      inputElement.classList.remove("active");
     }
     console.log(formData);
   };
 
-  const handleFamilyChange = (e) => {
-    setFormData({ ...formData, familyId: e.target.value });
-    
+  const handleGroupChange = (e) => {
+    setFormData({ ...formData, groupId: e.target.value });
   };
 
   useEffect(() => {
     if (userData) {
-      setUserFamilies(userData.user.families);
+      setUserGroups(userData.user.groups);
     }
   }, [userData]);
 
   useEffect(() => {
-    if (data && data.recipe) {
+    if (data && data.game) {
       // Set initial state using data
       setFormData({
-        name: data.recipe.name || "",
-        cookingTime: data.recipe.cookingTime || "",
-        servingSize: data.recipe.servingSize || "",
-        instructions: data.recipe.instructions || "",
-        ingredients: data.recipe.ingredients || "",
-        familyId: data.recipe.families._id || "",
+        name: data.game.name || "",
+        description: data.game.description || "",
+        castMembers: data.game.castMembers || "",
+        numMembers: data.game.numMembers || "",
+        groupId: data.game.groups._id || "",
       });
 
-      setMyImage(data.recipe.photo || ""); // Set image if available
+      setMyImage(data.game.photo || ""); // Set image if available
     }
 
-    document.querySelectorAll('input').forEach((input) => {
-        input.classList.add('active');
+    document.querySelectorAll("input").forEach((input) => {
+      input.classList.add("active");
     });
 
-    document.querySelectorAll('textarea').forEach((textarea) => {
-      textarea.classList.add('active');
-  });
-
+    document.querySelectorAll("textarea").forEach((textarea) => {
+      textarea.classList.add("active");
+    });
   }, [data]);
-
-  
 
   const handleUpload = (event) => {
     event.preventDefault();
@@ -147,49 +142,31 @@ export default function RecipeForm() {
 
   useEffect(() => {
     // Initialize MDB UI Kit components after the component has loaded
-    document.querySelectorAll('form-outline').forEach((formOutline) => {
+    document.querySelectorAll("form-outline").forEach((formOutline) => {
       if (formOutline && formOutline.classList) {
         try {
           new Input(formOutline).update();
         } catch (error) {
-          console.error('Error updating Input:', error);
+          console.error("Error updating Input:", error);
         }
       }
-    }
-    );
+    });
   }, []);
-  
 
   return (
     <form className="mb-5 p-1">
-       <div className="col m-auto">
-      <div className="col-5">
-      <div data-mdb-input-init className="form-outline mb-3">
-          <input
-            type="text"
-            id="name"
-            className="form-control"
-            value={formData.name}
-            onChange={handleInputChange}
-          />
-          <label className="form-label" htmlFor="name">
-            Recipe Name
-          </label>
-        </div>
-        </div>
-
-        <div className="col-5 p-0">
+      <div className="col m-auto">
+        <div className="col-5">
           <div data-mdb-input-init className="form-outline mb-3">
             <input
-              type="number"
-              id="cookingTime"
-              placeholder="0"
+              type="text"
+              id="name"
               className="form-control"
-              value={formData.cookingTime}
+              value={formData.name}
               onChange={handleInputChange}
             />
-            <label className="form-label" htmlFor="cookingTime">
-              Cooking Time (In Minutes)
+            <label className="form-label" htmlFor="name">
+              Game Name
             </label>
           </div>
         </div>
@@ -198,14 +175,30 @@ export default function RecipeForm() {
           <div data-mdb-input-init className="form-outline mb-3">
             <input
               type="number"
-              id="servingSize"
-              className="form-control"
+              id="description"
               placeholder="0"
-              value={formData.servingSize}
+              className="form-control"
+              value={formData.description}
               onChange={handleInputChange}
             />
-            <label className="form-label" htmlFor="servingSize">
-              Serving Size (Number of People)
+            <label className="form-label" htmlFor="description">
+              Whats this show about?
+            </label>
+          </div>
+        </div>
+
+        <div className="col-5 p-0">
+          <div data-mdb-input-init className="form-outline mb-3">
+            <input
+              type="number"
+              id="castMembers"
+              className="form-control"
+              placeholder="0"
+              value={formData.castMembers}
+              onChange={handleInputChange}
+            />
+            <label className="form-label" htmlFor="castMembers">
+              Name of cast members
             </label>
           </div>
         </div>
@@ -213,53 +206,42 @@ export default function RecipeForm() {
         <div data-mdb-input-init className="form-outline mb-3">
           <textarea
             className="form-control"
-            id="instructions"
+            id="numMembers"
             rows="4"
-            value={formData.instructions}
+            value={formData.numMembers}
             onChange={handleInputChange}
           ></textarea>
-          <label className="form-label" htmlFor="instructions">
-            Cooking Instructions (Separate each step with a new line)
-          </label>
-        </div>
-
-        <div data-mdb-input-init className="form-outline mb-3">
-          <textarea
-            className="form-control"
-            id="ingredients"
-            rows="4"
-            value={formData.ingredients}
-            onChange={handleInputChange}
-          ></textarea>
-          <label className="form-label" htmlFor="ingredients">
-            Ingredients (Separate each ingredient with a new line)
+          <label className="form-label" htmlFor="numMembers">
+            Insert # variable
           </label>
         </div>
 
         <div data-mdb-input-init className="form-outline m-auto row">
-          <label className="visually-hidden" >
-            Family
-          </label>
+          <label className="visually-hidden">Group</label>
           <select
             data-mdb-select-init
             className="select"
-            id="familyId"
-            placeholder="Select a family to share this recipe"
-            value={formData.familyId}
-            onChange={handleFamilyChange}
+            id="groupId"
+            placeholder="Select a group to share this game"
+            value={formData.groupId}
+            onChange={handleGroupChange}
           >
             {loading ? (
               <option defaultValue="" disabled className="optColor">
-              Join or create family to upload a new recipe
+                Join or create group to upload a new game
               </option>
             ) : (
               <>
                 <option defaultValue="" selected className="optColor">
-                Choose a family (Select a family to share this recipe)
+                  Choose a group (Select a group to share this game)
                 </option>
-                {userFamlies.map((family) => (
-                  <option key={family._id} value={family._id} className="optColor">
-                    {family.name}
+                {userFamlies.map((group) => (
+                  <option
+                    key={group._id}
+                    value={group._id}
+                    className="optColor"
+                  >
+                    {group.name}
                   </option>
                 ))}
               </>
@@ -272,15 +254,23 @@ export default function RecipeForm() {
           className="form-outline mt-4 m-auto row"
           onClick={handleUpload}
         >
-        <section>
-          <div >
-              <img className="uploaded-image-cloudinary" src={myImage} id="cloudBox"/>
-          </div>
-          <div className="m-auto">
-              <button className="" onClick={() => widgetRef.current.open()}>Upload Image</button>
-          </div>
-        </section>
-          <sub className="text-muted m-auto mt-2">Upload a picture of your recipe</sub>
+          <section>
+            <div>
+              <img
+                className="uploaded-image-cloudinary"
+                src={myImage}
+                id="cloudBox"
+              />
+            </div>
+            <div className="m-auto">
+              <button className="" onClick={() => widgetRef.current.open()}>
+                Upload Image
+              </button>
+            </div>
+          </section>
+          <sub className="text-muted m-auto mt-2">
+            Upload a picture of your game
+          </sub>
         </div>
 
         <button
@@ -289,10 +279,12 @@ export default function RecipeForm() {
           className="btn btn-block btn-lg m-auto mt-4 submit"
           onClick={handleSubmit}
         >
-          Update Recipe
+          Update Game
         </button>
       </div>
-      <h4 className="text-center" style={{color: "red"}}>{uploadError}</h4>
+      <h4 className="text-center" style={{ color: "red" }}>
+        {uploadError}
+      </h4>
     </form>
   );
 }
