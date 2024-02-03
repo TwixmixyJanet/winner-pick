bde053f7f39feebbcb9e86786796b6796a42e463
Janet Webster <twixmixy@gmail.com>
2024-02-03 10:34:49 -0500

    Refactor editrecipe to be editgame

---
+++ client/src/pages/EditGame.jsx
@@ -0,0 +1,12 @@
+import EditGameCard from "../components/EditGameCard";
+import "./style.css";
+
+function EditGame() {
+  return (
+    <main className="md-container m-auto text-center" id="cardContainer">
+      <EditGameCard />
+    </main>
+  );
+}
+
+export default EditGame;
