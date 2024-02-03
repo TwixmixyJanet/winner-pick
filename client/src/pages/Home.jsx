26356cc5629d478c965a38c87641b437903b446b
Janet Webster <twixmixy@gmail.com>
2024-02-03 10:59:00 -0500

    Update Home page to display top games instead of top recipes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,17 +1,17 @@
-import RecipeCard from '../components/RecipeCard';
-import Carousels from '../components/Carousel';
-import './style.css';
+import GameCard from "../components/GameCard";
+import Carousels from "../components/Carousel";
+import "./style.css";
 
 function Home() {
-    return (
-        <section className="md-container m-auto text-center">
-         <Carousels />
-         <h2 className="mt-5 mb-1">✨ Top Recipes ✨</h2>
-        <div className="d-flex p-3 flex-wrap" id="cardContainer">
-            <RecipeCard />
-        </div>
-        </section>
-    );
+  return (
+    <section className="md-container m-auto text-center">
+      <Carousels />
+      <h2 className="mt-5 mb-1">✨ Top Games ✨</h2>
+      <div className="d-flex p-3 flex-wrap" id="cardContainer">
+        <GameCard />
+      </div>
+    </section>
+  );
 }
 
 export default Home;
