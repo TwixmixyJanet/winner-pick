9cd1592c3069a6af35b4b6381a7fb7e9dfe4c2bf
Janet Webster <twixmixy@gmail.com>
2024-02-03 11:23:56 -0500

    Change Recipe page to be Game page

--- client/src/pages/Game.jsx
+++ client/src/pages/Game.jsx
@@ -3,45 +3,40 @@ import { useEffect, useState } from "react";
 import { Link, useParams, useNavigate } from "react-router-dom";
 import { useQuery, useLazyQuery, useMutation } from "@apollo/client";
 
-import { QUERY_RECIPE, QUERY_USER } from "../utils/queries";
-import { PIN_RECIPE, UNPIN_RECIPE } from "../utils/mutations";
+import { QUERY_GAME, QUERY_USER } from "../utils/queries";
+import { JOIN_GAME, LEAVE_GAME } from "../utils/mutations";
 import "./style.css";
 import Auth from "../utils/auth";
 
-function Recipe() {
-  const { id: recipeId } = useParams();
+function Game() {
+  const { id: gameId } = useParams();
   const navigate = useNavigate();
 
-  const [recipe, setRecipe] = useState({});
-  const [family, setFamily] = useState({});
+  const [game, setGame] = useState({});
+  const [group, setGroup] = useState({});
   const loggedIn = Auth.loggedIn();
-  const [pinned, setPinned] = useState(false);
+  const [joined, setJoined] = useState(false);
 
-  const [pinRecipe] = useMutation(PIN_RECIPE);
-  const [unpinRecipe] = useMutation(UNPIN_RECIPE);
+  const [joinGame] = useMutation(JOIN_GAME);
+  const [leaveGame] = useMutation(LEAVE_GAME);
 
-  const { loading, data, error } = useQuery(QUERY_RECIPE, {
-    variables: { id: recipeId },
+  const { loading, data, error } = useQuery(QUERY_GAME, {
+    variables: { id: gameId },
   });
 
   useEffect(() => {
     if (data) {
-      setRecipe(data.recipe);
+      setGame(data.game);
     }
   }, [data, loading, error]);
 
   useEffect(() => {
-    if (
-      data &&
-      data.recipe &&
-      data.recipe.families &&
-      data.recipe.families.name
-    ) {
-      setFamily(data.recipe.families);
+    if (data && data.game && data.game.groups && data.game.groups.name) {
+      setGroup(data.game.groups);
     }
   }, [data, loading, error]);
 
-  const handleReturnToRecipes = () => {
+  const handleReturnToGames = () => {
     // Use the navigate function to navigate back
     navigate(-1); // This is equivalent to navigating back one step
   };
@@ -61,94 +56,86 @@ function Recipe() {
       userData &&
       userData.data &&
       userData.data.user &&
-      userData.data.user.pinnedRecipes
+      userData.data.user.joinedGames
     ) {
-      if (
-        userData.data.user.pinnedRecipes.find(
-          (recipe) => recipe._id === recipeId
-        )
-      ) {
-        setPinned(true);
+      if (userData.data.user.joinedGames.find((game) => game._id === gameId)) {
+        setJoined(true);
       }
     }
-  }, [userData, recipeId]);
+  }, [userData, gameId]);
 
-  const pinHandler = async () => {
-    const { data } = await pinRecipe({
-      variables: { id: recipeId },
+  const joinHandler = async () => {
+    const { data } = await joinGame({
+      variables: { id: gameId },
     });
-    setPinned(true);
+    setJoined(true);
   };
 
-  const unpinHandler = async () => {
-    const { data } = await unpinRecipe({
-      variables: { id: recipeId },
+  const leaveHandler = async () => {
+    const { data } = await leaveGame({
+      variables: { id: gameId },
     });
-    setPinned(false);
+    setJoined(false);
   };
 
   return (
     <>
-      {recipe ? (
+      {game ? (
         <div>
           <div className="m-4 d-flex justify-content-center align-items-center">
             <div className="card w-75">
               <img
-                src={recipe.photo}
-                alt={recipe.name}
+                src={game.photo}
+                alt={game.name}
                 className="card-img-top"
-                id="recipe-image"
+                id="game-image"
               />
               <div className="card-body">
-                <h1 className="card-title mb-5 mt-3">{recipe.name}</h1>
+                <h1 className="card-title mb-5 mt-3">{game.name}</h1>
                 <div className="card-text mb-5">
                   <div className="row">
                     <div className="col-md-6 mb-1">
-                        <div className="field-title m-0">Cooking Time:</div>{" "}
-                        {recipe.cookingTime} minutes
+                      <div className="field-title m-0">Description:</div>{" "}
+                      {game.description} context
                     </div>
                     <div className="col-md-6 mb-1">
-                        <div className="field-title m-0">Serving Size:</div>{" "}
-                        {recipe.servingSize} servings
+                      <div className="field-title m-0">Cast Members:</div>{" "}
+                      {game.castMembers} members
                     </div>
                     <div className="col-md-6 mt-5 mb-2">
                       <div className="field-title m-0  d-flex text-start">
-                        Cooking Instructions:{" "}
+                        Number of Members:{" "}
                       </div>
-                     <div className="textwrap d-flex text-start">{recipe.instructions}
-                    </div></div> 
-                    <div className="col-md-6 mt-5 mb-2">
-                      <div className="field-title m-0 d-flex text-start">Ingredients: </div>{" "}
-                     <div className="textwrap d-flex text-start">{recipe.ingredients}</div> 
+                      <div className="textwrap d-flex text-start">
+                        {game.numMembers}
+                      </div>
                     </div>
                   </div>
                 </div>
                 <div className="d-flex justify-content-center">
-                  <div className="row" >
+                  <div className="row">
                     <div className="col m-auto p-1">
-                      <Link to={`/familyrecipes/${family?._id}`}>
-                        <div className="badge family-badge p-auto m-2">
+                      <Link to={`/groupgames/${group?._id}`}>
+                        <div className="badge group-badge p-auto m-2">
                           <i className="fas fa-users fa-lg m-2"></i>
-                          <p className=" m-0">Family:{" "}
-                           {family?.name || ""}</p>
+                          <p className=" m-0">Group: {group?.name || ""}</p>
                         </div>
                       </Link>
                     </div>
                     <div className="col m-auto p-1">
-                       <div className="badge author-badge p-auto m-2">
-                          <i className="fas fa-user fa-lg m-2"></i>
-                          <p className=" m-0">Author:{" "}
-                          {recipe.author}</p>
-                        </div>
+                      <div className="badge author-badge p-auto m-2">
+                        <i className="fas fa-user fa-lg m-2"></i>
+                        <p className=" m-0">Author: {game.author}</p>
+                      </div>
                     </div>
                     <div className="col m-auto p-1">
                       <div>
                         {loggedIn !== false ? (
                           <>
-                            {pinned !== false ? (
+                            {joined !== false ? (
                               <div
                                 className="badge badge-light p-auto"
-                                onClick={unpinHandler}
+                                onClick={leaveHandler}
                               >
                                 <i
                                   className="fa-solid fa-thumbtack fa-2x p-auto"
@@ -158,7 +145,7 @@ function Recipe() {
                             ) : (
                               <div
                                 className="badge badge-light p-auto"
-                                onClick={pinHandler}
+                                onClick={joinHandler}
                               >
                                 <i
                                   className="fa-solid fa-thumbtack fa-2x p-auto"
@@ -178,14 +165,13 @@ function Recipe() {
             </div>
           </div>
           <div className="m-4">
-            {/* If we could find a way for this to return to previous page instead of home, that would be better. */}
             <Link to="/">
               <div
                 className="badge bg-secondary"
-                onClick={handleReturnToRecipes}
+                onClick={handleReturnToGames}
                 style={{ cursor: "pointer" }}
               >
-                <i className="fas fa-arrow-left-long"></i> Return to Recipes
+                <i className="fas fa-arrow-left-long"></i> Return to Games
               </div>
             </Link>
           </div>
@@ -196,4 +182,4 @@ function Recipe() {
   );
 }
 
-export default Recipe;
+export default Game;
