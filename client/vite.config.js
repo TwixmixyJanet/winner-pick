aaa0e4e03e04daa02f4231f62f58cd5796a97346
Janet Webster <twixmixy@gmail.com>
2024-02-03 12:55:20 -0500

    Update vite.config.js formatted

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,11 +8,11 @@ export default defineConfig({
     port: 3000,
     open: true,
     proxy: {
-      '/graphql': {
-        target: 'http://localhost:3001',
+      "/graphql": {
+        target: "http://localhost:3001",
         secure: false,
-        changeOrigin: true
-      }
-    }
-  }
-})
+        changeOrigin: true,
+      },
+    },
+  },
+});
