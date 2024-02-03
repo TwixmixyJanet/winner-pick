e7c002a923878f97b9ba78e4e92fa67c9c2eaec8
Janet Webster <twixmixy@gmail.com>
2024-02-03 10:13:15 -0500

    Change add recipe to add game page

---
+++ client/src/pages/AddGame.jsx
@@ -0,0 +1,12 @@
+import GameForm from "../components/GameForm";
+import "./style.css";
+
+function AddGame() {
+  return (
+    <main className="md-container m-auto text-center" id="cardContainer">
+      <GameForm />
+    </main>
+  );
+}
+
+export default AddGame;
