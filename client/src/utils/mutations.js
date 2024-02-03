0adc96a6ff6b9be5c2d8f46f63eeca71c7d7f3db
Janet Webster <twixmixy@gmail.com>
2024-02-03 10:11:11 -0500

    Refactor mutations.js to be group and game

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,157 +1,199 @@
 // Important for useMutation: We bring in gql from the @apollo/client library to allow us to parse mutations (and queries) as template literals
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
 // Important for useMutation: Each mutation we'd like to be able to perform gets exported out of our mutations.js utility
 
 export const LOGIN = gql`
-    mutation login($username: String!, $password: String!) {
-        login(username: $username, password: $password) {
-            token
-            user {
-                _id
-                email
-                password
-            }
-        }
+  mutation login($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
+      token
+      user {
+        _id
+        email
+        password
+      }
     }
+  }
 `;
 
 export const ADD_USER = gql`
-    mutation addUser($firstName: String!, $lastName: String!, $username: String!, $email: String!, $password: String!) {
-        addUser(firstName: $firstName, lastName: $lastName, username: $username, email: $email, password: $password) {
-            token
-            user {
-                _id
-                email
-                firstName
-                lastName
-                username
-            }
-        }
+  mutation addUser(
+    $firstName: String!
+    $lastName: String!
+    $username: String!
+    $email: String!
+    $password: String!
+  ) {
+    addUser(
+      firstName: $firstName
+      lastName: $lastName
+      username: $username
+      email: $email
+      password: $password
+    ) {
+      token
+      user {
+        _id
+        email
+        firstName
+        lastName
+        username
+      }
     }
+  }
 `;
 
-
-export const ADD_FAMILY = gql`
-    mutation addFamily($name: String!) {
-        addFamily(name: $name) {
-            _id
-            name   
-        }
+export const ADD_GROUP = gql`
+  mutation addGroup($name: String!) {
+    addGroup(name: $name) {
+      _id
+      name
     }
+  }
 `;
 
-export const JOIN_FAMILY = gql`
-    mutation joinFamily($familyId: ID!) {
-        joinFamily(familyId: $familyId) {
-            _id
-            firstName
-            lastName
-            username
-            email
-            families {
-                _id
-                name
-            }
-        }
+export const JOIN_GROUP = gql`
+  mutation joinGroup($groupId: ID!) {
+    joinGroup(groupId: $groupId) {
+      _id
+      firstName
+      lastName
+      username
+      email
+      groups {
+        _id
+        name
+      }
     }
+  }
 `;
 
-export const LEAVE_FAMILY = gql`
-    mutation leaveFamily($familyId: ID!) {
-        leaveFamily(familyId: $familyId) {
-            _id
-            firstName
-            lastName
-            username
-            email
-            families {
-                _id
-                name
-            }
-        }
+export const LEAVE_GROUP = gql`
+  mutation leaveGroup($groupId: ID!) {
+    leaveGroup(groupId: $groupId) {
+      _id
+      firstName
+      lastName
+      username
+      email
+      groups {
+        _id
+        name
+      }
     }
-`
+  }
+`;
 
-export const ADD_RECIPE = gql`
-    mutation addRecipe($name: String!, $photo: String!, $cookingTime: String!, $instructions: String!, $ingredients: String!, $servingSize: String!, $author: String!, $familyId: ID) {
-        addRecipe(name: $name, photo: $photo, cookingTime: $cookingTime, instructions: $instructions, ingredients: $ingredients, servingSize: $servingSize, author: $author, familyId: $familyId) {
-            _id
-            name
-            photo
-            cookingTime
-            instructions
-            ingredients
-            servingSize
-            author
-            createdAt
-            families {
-                _id
-            }
-        }
+export const ADD_GAME = gql`
+  mutation addGame(
+    $name: String!
+    $photo: String!
+    $description: String!
+    $castMembers: String!
+    $numMembers: String!
+    $author: String!
+    $groupId: ID
+  ) {
+    addGame(
+      name: $name
+      photo: $photo
+      description: $description
+      castMembers: $castMembers
+      numMembers: $numMembers
+      author: $author
+      groupId: $groupId
+    ) {
+      _id
+      name
+      photo
+      description
+      castMembers
+      numMembers
+      author
+      createdAt
+      groups {
+        _id
+      }
     }
-`
+  }
+`;
 
-export const UPDATE_RECIPE = gql`
-    mutation updateRecipe($id: ID!, $cookingTime: String!, $servingSize: String!, $name: String, $photo: String, $instructions: String, $ingredients: String, $author: String, $familyId: ID) {
-        updateRecipe(_id: $id, cookingTime: $cookingTime, servingSize: $servingSize, name: $name, photo: $photo, instructions: $instructions, ingredients: $ingredients, author: $author, familyId: $familyId) {
-          _id
-          name
-          photo
-          cookingTime
-          instructions
-          ingredients
-          servingSize
-          author
-          createdAt
-          families {
-            _id
-            name
-          }
-        }
+export const UPDATE_GAME = gql`
+  mutation updateGame(
+    $id: ID!
+    $name: String!
+    $photo: String!
+    $description: String!
+    $castMembers: String!
+    $numMembers: String!
+    $author: String!
+    $groupId: ID
+  ) {
+    updateGame(
+      _id: $id
+      name: $name
+      photo: $photo
+      description: $description
+      castMembers: $castMembers
+      numMembers: $numMembers
+      author: $author
+      groupId: $groupId
+    ) {
+      _id
+      name
+      photo
+      description
+      castMembers
+      numMembers
+      author
+      createdAt
+      groups {
+        _id
+        name
+      }
     }
-`
+  }
+`;
 
-export const DELETE_RECIPE = gql`
-    mutation deleteRecipe($id: ID!) {
-        deleteRecipe(_id: $id) {
-            _id
-            name
-            photo
-            cookingTime
-            instructions
-            ingredients
-            servingSize
-            author
-            createdAt
-            families {
-                _id
-                name
-            }
-        }
+export const DELETE_GAME = gql`
+  mutation deleteGame($id: ID!) {
+    deleteGame(_id: $id) {
+      _id
+      name
+      photo
+      description
+      castMembers
+      numMembers
+      author
+      createdAt
+      groups {
+        _id
+        name
+      }
     }
-`
+  }
+`;
 
-export const PIN_RECIPE = gql`
-    mutation pinRecipe($id: ID!) {
-        pinRecipe(_id: $id) {
-            _id
-            username
-            pinnedRecipes {
-                _id
-            }
-        }
+export const JOIN_GAME = gql`
+  mutation joinGame($id: ID!) {
+    joinGame(_id: $id) {
+      _id
+      username
+      joinedGames {
+        _id
+      }
     }
-`
+  }
+`;
 
-export const UNPIN_RECIPE = gql`
-    mutation unpinRecipe($id: ID!) {
-        unpinRecipe(_id: $id) {
-            _id
-            username
-            pinnedRecipes {
-                _id
-            }
-        }
+export const LEAVE_GAME = gql`
+  mutation leaveGame($id: ID!) {
+    leaveGame(_id: $id) {
+      _id
+      username
+      joinedGames {
+        _id
+      }
     }
+  }
 `;
