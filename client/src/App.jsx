98909d03dbae56d8dfca36a83333f62375e90aa9
Janet Webster <twixmixy@gmail.com>
2024-02-03 09:39:11 -0500

    Added format on save, so files with auto format

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,30 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet } from "react-router-dom";
 import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
   createHttpLink,
-} from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
-import NavBar from './components/Navbar';
-import Footer from './components/Footer';
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import NavBar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 // Construct request middleware that will attach the JWT token to every request as an `authorization` header
 const authLink = setContext((_, { headers }) => {
   // get the authentication token from local storage if it exists
-  const token = localStorage.getItem('id_token');
+  const token = localStorage.getItem("id_token");
   // return the headers to the context so httpLink can read them
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : '',
+      authorization: token ? `Bearer ${token}` : "",
     },
   };
 });
 
 // Construct our main GraphQL API endpoint
 const httpLink = createHttpLink({
-  uri: '/graphql',
+  uri: "/graphql",
 });
 
 const client = new ApolloClient({
@@ -36,10 +36,9 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-        <NavBar />
-        <Outlet />
-        <Footer />
-
+      <NavBar />
+      <Outlet />
+      <Footer />
     </ApolloProvider>
   );
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,80 +1,77 @@
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App.jsx';
-import Home from './pages/Home.jsx';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import Home from "./pages/Home.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-
-import Dashboard from './pages/Dashboard.jsx';
-import AddRecipe from './pages/AddRecipe.jsx';
-import FamilyRecipes from './pages/FamilyRecipes.jsx';
-import Search from './pages/Search.jsx';
-import Login from './pages/Login.jsx';
-import Recipe from './pages/Recipe.jsx';
-import EditRecipe from './pages/EditRecipe.jsx';
-import PinnedRecipes from './pages/PinnedRecipe.jsx'
+import Dashboard from "./pages/Dashboard.jsx";
+import AddRecipe from "./pages/AddRecipe.jsx";
+import FamilyRecipes from "./pages/FamilyRecipes.jsx";
+import Search from "./pages/Search.jsx";
+import Login from "./pages/Login.jsx";
+import Recipe from "./pages/Recipe.jsx";
+import EditRecipe from "./pages/EditRecipe.jsx";
+import PinnedRecipes from "./pages/PinnedRecipe.jsx";
 // Import MDB
 // Example: Adjust import path based on package structure or documentation
-import * as mdb from 'mdb-ui-kit'; // lib
-
+import * as mdb from "mdb-ui-kit"; // lib
 
 window.mdb = mdb;
 
-
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <App />,
     // error: <NoMatch />,
     children: [
       {
-        index: true, 
-        element: <Home />
+        index: true,
+        element: <Home />,
       },
       {
-        path:'/dashboard',
-        index: false, 
-        element: <Dashboard />
+        path: "/dashboard",
+        index: false,
+        element: <Dashboard />,
       },
       {
-        path:'/addrecipe',
-        index: false, 
-        element: <AddRecipe />
+        path: "/addrecipe",
+        index: false,
+        element: <AddRecipe />,
       },
       {
-        path:'/login',
-        index: false, 
-        element: <Login />
+        path: "/login",
+        index: false,
+        element: <Login />,
       },
       {
-        path:'/recipe/:id',
-        index: false, 
-        element: <Recipe />
+        path: "/recipe/:id",
+        index: false,
+        element: <Recipe />,
       },
       {
-        path:'/familyrecipes/:familyId',
-        index: false, 
-        element: <FamilyRecipes />
+        path: "/familyrecipes/:familyId",
+        index: false,
+        element: <FamilyRecipes />,
       },
       {
-        path:'/editrecipe/:id',
-        index: false, 
-        element: <EditRecipe />
+        path: "/editrecipe/:id",
+        index: false,
+        element: <EditRecipe />,
       },
       {
-        path:'/search/',
-        index: false, 
-        element: <Search />
+        path: "/search/",
+        index: false,
+        element: <Search />,
       },
       {
-        path:'/pinnedrecipes/',
-        index: false, 
-        element: <PinnedRecipes />
-      }
-    ]
-  }
+        path: "/pinnedrecipes/",
+        index: false,
+        element: <PinnedRecipes />,
+      },
+    ],
+  },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <RouterProvider router={router} />
-)
+);
