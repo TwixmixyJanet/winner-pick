725c180136b775e0f68f9035816b6dbfff7f64ce
Janet Webster <twixmixy@gmail.com>
2024-02-03 11:58:59 -0500

    Update footer copyright year

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -2,14 +2,13 @@ import { Input, initMDB } from "mdb-ui-kit";
 
 initMDB({ Input });
 
-
 export const Footer = () => {
-    return (
-        <footer>
-          <div>© 2023 Copyright Recipe Rolodex</div>
-          {/* <a className="text-body" href="https://mdbootstrap.com/">MDBootstrap.com</a> */}
-      </footer>
-    )
-}
+  return (
+    <footer>
+      <div>© 2024 Copyright 🔥 WINNER PICK 🔥</div>
+      {/* <a className="text-body" href="https://mdbootstrap.com/">MDBootstrap.com</a> */}
+    </footer>
+  );
+};
 
 export default Footer;
