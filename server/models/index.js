1e12c250b38c19aaefd572347c7c5d499e8aac0e
Janet Webster <twixmixy@gmail.com>
2024-02-06 15:50:12 -0500

    Add CastMember and Episode models

--- server/models/index.js
+++ server/models/index.js
@@ -1,6 +1,7 @@
-const User = require('./User');
-const Game = require('./Game');
-const Group = require('./Group');
+const User = require("./User");
+const Game = require("./Game");
+const Group = require("./Group");
+const CastMember = require("./CastMember");
+const Episode = require("./Episode");
 
-
-module.exports = { User, Game, Group};
+module.exports = { User, Game, Group, CastMember, Episode };
