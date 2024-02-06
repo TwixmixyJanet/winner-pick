f41ef284b9babcd468838b68c4e47f4d2de2ab79
Janet Webster <twixmixy@gmail.com>
2024-02-06 15:29:47 -0500

    Refactor User model schema
    This commit refactors the User model schema in the server/models/User.js file. It updates the import statements to use double quotes instead of single quotes. It also adds missing commas after the trim property in the username, firstName, and lastName fields. Additionally, it adds a minlength property to the password field. The groups, games, castMembers, and joinedGames fields are updated to use an array of objects with a type and ref property. Finally, the pre-save middleware function is updated to use double quotes instead of single quotes.

--- server/models/User.js
+++ server/models/User.js
@@ -1,59 +1,66 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
-const bcrypt = require('bcrypt');
-const Game = require('./Game');
-const Group = require('./Group');
+const bcrypt = require("bcrypt");
+const Game = require("./Game");
+const Group = require("./Group");
 
 const userSchema = new Schema({
   username: {
     type: String,
     required: true,
     unique: true,
-    trim: true
+    trim: true,
   },
   firstName: {
     type: String,
     required: true,
-    trim: true
+    trim: true,
   },
   lastName: {
     type: String,
     required: true,
-    trim: true
+    trim: true,
   },
   email: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
   },
   password: {
     type: String,
     required: true,
-    minlength: 5
+    minlength: 5,
   },
-  groups: [{
-    type: Schema.Types.ObjectId,
-    ref: 'Group'
-  },
+  groups: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Group",
+    },
   ],
   games: [
     {
       type: Schema.Types.ObjectId,
-      ref: 'Game'
+      ref: "Game",
+    },
+  ],
+  castMembers: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "CastMember",
     },
   ],
   joinedGames: [
     {
       type: Schema.Types.ObjectId,
-      ref: 'Game'
+      ref: "Game",
     },
-  ]
+  ],
 });
 
 // set up pre-save middleware to create password
-userSchema.pre('save', async function (next) {
-  if (this.isNew || this.isModified('password')) {
+userSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
     const saltRounds = 10;
     this.password = await bcrypt.hash(this.password, saltRounds);
   }
@@ -66,6 +73,6 @@ userSchema.methods.isCorrectPassword = async function (password) {
   return await bcrypt.compare(password, this.password);
 };
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
